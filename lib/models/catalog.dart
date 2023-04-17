b176280591e315e242a3067fc8eb116f5a107bee
hrishee-singh <hrisheekesh.patoda@gmail.com>
2023-04-17 15:22:22 +0530

    day 10 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,21 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(this.id, this.name, this.desc, this.price, this.color, this.image);
+}
+
+final products = [
+Item(
+id: "101",
+name: "iPhone 14 Pro",
+desc: "Apple iPhone 14th Gen",
+price: 999,
+color: "#33505a",
+image: "https://w7.pngwing.com/pngs/961/642/png-transparent-iphone-14-pro.png",
+
+)];
