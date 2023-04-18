04e991b810f3112b8c8e2cd66332b54e6fb464a9
hrishee-singh <hrisheekesh.patoda@gmail.com>
2023-04-18 23:50:42 +0530

    day 12 commit

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,3 +1,17 @@
+class CatalogModel {
+  static final items = [
+    Item(
+      id: "101",
+      name: "iPhone 14 Pro",
+      desc: "Apple iPhone 14th Gen",
+      price: 999,
+      color: "#33505a",
+      image:
+          "https://w7.pngwing.com/pngs/60/414/png-transparent-iphone-14-thumbnail.png",
+    )
+  ];
+}
+
 class Item {
   final String id;
   final String name;
@@ -6,16 +20,11 @@ class Item {
   final String color;
   final String image;
 
-  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
-}
-
-final products = [
   Item(
-    id: "101",
-    name: "iPhone 14 Pro",
-    desc: "Apple iPhone 14th Gen",
-    price: 999,
-    color: "#33505a",
-image: "https://w7.pngwing.com/pngs/961/642/png-transparent-iphone-14-pro.png",
-
-)];
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
+}
--- lib/pages/home_page1.dart
+++ lib/pages/home_page1.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:s/models/catalog.dart';
 import 'package:s/widgets/drawer.dart';
 
+import '../widgets/item_widget.dart';
+
 class HomePage extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-    const int days = 30;
-    const String name = "Codepur";
-
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
       ),
-      body: const Center(
-        child: Text("Welcome to $days days of Flutter by $name "),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
+        ),
       ),
       drawer: MyDrawer(),
     );
